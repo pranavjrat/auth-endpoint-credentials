c2b09e37729c363e66cbac699e7c0d0e001f8136
pranavjrat <pranavjrat@gmail.com>
2025-05-17 02:35:52 +0530

    cc

--- db/index.js
+++ db/index.js
@@ -4,11 +4,7 @@ import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from '../config.js';
 const { Pool } = pg;
 
 const pool = new Pool({
-  port: DB_PORT,
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  connectionString: process.env.DB_URL,
 });
 
 const db = {
